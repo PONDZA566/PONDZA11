3529edb24ab05035529cc30adb4961bc5df02e8a
PONDZA <thanat_t@cmu.ac.th>
2023-01-03 16:36:04 +0700

    lab11_completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,17 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	bool havetext;
+	havetext = getline(source,textline);
+	dest << "-------------------- BOOM ---------------------\n";
+	
+	while(havetext){
+		dest << textline << "\n";
+		havetext = getline(source,textline);
+	}
+
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0,sum2 = 0,mean,std_dev;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        count++;
+        sum2 += pow(atof(textline.c_str()),2);
+    }
+    mean = sum/count;
+    std_dev = sqrt((1.0/count)*sum2-pow(mean,2));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << std_dev;
+    source.close();
+    return 0;
+
+
+}
